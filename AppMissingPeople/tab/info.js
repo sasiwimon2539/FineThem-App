3365f2f11f2407d2e698fda335e397446931c410
SRP <sp_ray9731@rmutsvmail.com>
2018-01-21 15:36:26 +0700

    list

--- AppMissingPeople/tab/info.js
+++ AppMissingPeople/tab/info.js
@@ -12,7 +12,7 @@ export default class Info extends React.Component{
     render() {
         return  <View style={styles.container}>
             <Text style={styles.welcome}>
-                This info page
+                This profile page
             </Text>
         </View>
     }
